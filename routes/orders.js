74687a9f7200e6de260ff29a6f0ea5ed98919b96
lelonS <35070147+lelonS@users.noreply.github.com>
2023-04-19 20:06:16 +0200

    Add orders routes

--- routes/orders.js
+++ routes/orders.js
@@ -5,5 +5,125 @@ import { errorResponse, notFoundResponse } from '../errors/error-messages.js';
 
 const ordersRouter = Router();
 
+const orderSchema = new Schema({
+  customer: { type: Schema.Types.ObjectId, ref: 'Customers', required: true },
+  shippingAddress: { type: String, required: true },
+  products: [{
+    product: { type: Schema.Types.ObjectId, ref: 'Products', required: true },
+    quantity: { type: Number, required: true, min: 1 }
+  }],
+  createdAt: { type: Date, default: Date.now }
+});
+
+const Orders = mongoose.model('Orders', orderSchema);
+
+
+// Post a new order
+ordersRouter.post('/', async (req, res) => {
+  // Create a new order
+  const order = new Orders(req.body);
+
+  // Save the order
+  order.save()
+    .then((result) => {
+      // Save successful
+      res.status(201).send(result);
+    })
+    .catch((err) => {
+      // Save failed. Get the status code and send the error message
+      const errorMessage = errorResponse(err);
+      res.status(errorMessage.status).send(errorMessage.response);
+    });
+});
+
+
+// Get all orders
+ordersRouter.get('/', async (req, res) => {
+  // Get all orders
+  Orders.find()
+    .populate('customer')
+    .populate('products.product')
+    .then((result) => {
+      // Get successful
+      res.send(result);
+    })
+    .catch((err) => {
+      // Get failed. Get the status code and send the error message
+      const errorMessage = errorResponse(err);
+      res.status(errorMessage.status).send(errorMessage.response);
+    });
+});
+
+
+// Get an order by id
+ordersRouter.get('/:id', async (req, res) => {
+  // Get the order by id
+  Orders.findById(req.params.id)
+    .populate('customer')
+    .populate('products.product')
+    .then((result) => {
+      // Get successful
+      if (result) {
+        res.send(result);
+      } else {
+        // Order not found
+        const errorMessage = notFoundResponse('Order');
+        res.status(errorMessage.status).send(errorMessage.response);
+      }
+    })
+    .catch((err) => {
+      // Get failed. Get the status code and send the error message
+      const errorMessage = errorResponse(err);
+      res.status(errorMessage.status).send(errorMessage.response);
+    });
+});
+
+
+// Update an order by id
+ordersRouter.put('/:id', async (req, res) => {
+  // Update the order by id
+  Orders.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    .then((result) => {
+      // Update successful
+      if (result) {
+        res.send(result);
+      } else {
+        // Order not found
+        const errorMessage = notFoundResponse('Order');
+        res.status(errorMessage.status).send(errorMessage.response);
+      }
+    })
+    .catch((err) => {
+      // Update failed. Get the status code and send the error message
+      const errorMessage = errorResponse(err);
+      res.status(errorMessage.status).send(errorMessage.response);
+    });
+});
+
+
+// Delete an order by id
+ordersRouter.delete('/:id', async (req, res) => {
+  // Delete the order by id
+  Orders.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      // Delete successful
+      if (result) {
+        res.send(result);
+      } else {
+        // Order not found
+        const errorMessage = notFoundResponse('Order');
+        res.status(errorMessage.status).send(errorMessage.response);
+      }
+    })
+    .catch((err) => {
+      // Delete failed. Get the status code and send the error message
+      const errorMessage = errorResponse(err);
+      res.status(errorMessage.status).send(errorMessage.response);
+    });
+});
+
+
+
+
 
 export default ordersRouter;
